2c70ab2ec9f172124c51817ac38fb0e51c5813f3
iria <iria.pardo.neira@udc.es>
2023-03-04 17:57:45 +0100

    Clase ProblemaCuadradoMagico acabada 1st try

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/e2_a/ProblemaCuadradoMagico.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/e2_a/ProblemaCuadradoMagico.java
@@ -5,6 +5,10 @@ import es.udc.sistemasinteligentes.Estado;
 import es.udc.sistemasinteligentes.ProblemaBusqueda;
 import es.udc.sistemasinteligentes.ejemplo.ProblemaAspiradora;
 
+import java.util.ArrayList;
+
+import static java.lang.Math.pow;
+
 public class ProblemaCuadradoMagico extends ProblemaBusqueda {
     public static class EstadoCuadradoMagico extends Estado{
 
@@ -16,6 +20,11 @@ public class ProblemaCuadradoMagico extends ProblemaBusqueda {
             this.board = board;
             this.size = board.length;
 
+        }
+
+        public EstadoCuadradoMagico InsertarNumero (int numero, int fila, int columna){
+            board[fila][columna] = numero;
+            return new EstadoCuadradoMagico(board);
         }
 
         @Override
@@ -53,19 +62,91 @@ public class ProblemaCuadradoMagico extends ProblemaBusqueda {
             return result;
         }
     }
-    public static class AccionCuadradoMagico extends Accion{
+    public static class AccionCuadradoMagico extends Accion {
+        int valor;
+        int fila;
+        int columna;
 
-
-        public boolean esAplicable(Estado es) {
-            /*
-             * comprobamos si
-             * @param es  cumple las precondiciones para aplicar la acción
-             * @return true si en el tablero no está el número que se quiere insertar y que ese número está entre 1 y N^2
-             * @return false en caso contrario
-             */
+        public AccionCuadradoMagico(int valor, int fila, int columna) {
+            this.valor = valor;
+            this.fila = fila;
+            this.columna = columna;
         }
 
+        @Override
+        public String toString() {
+            return valor + " -> (i=" + fila + ",j=" + columna + ")";
+        }
 
+        public boolean esAplicable(Estado es) {
+            EstadoCuadradoMagico s = (EstadoCuadradoMagico)es;
+            int size = s.size;
+            //if (valor < 1 || valor < size) return false; NO hace falta esta precondición, se controla en el met acciones
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (s.board[i][j] == valor) //Si el número ya existe en el tablero
+                        return false;
+                }
+            }
+            return true;
+        }
+        @Override
+        public Estado aplicaA(Estado es){
+            EstadoCuadradoMagico nuevoEstado;
+            EstadoCuadradoMagico s = (EstadoCuadradoMagico)es;
+            nuevoEstado = s.InsertarNumero(valor, fila, columna);
+            return nuevoEstado;
+        }
     }
 
+    public ProblemaCuadradoMagico(Estado estadoInicial) {
+        super(estadoInicial);
+    }
+    @Override
+    public boolean esMeta(Estado es){
+        EstadoCuadradoMagico s = (EstadoCuadradoMagico)es;
+        int size = s.size;
+        int sum_col = 0;
+        int sum_row = 0;
+        int sum_d1 = 0;
+        int sum_d2 = 0;
+        int sumaMagica = size * ((int)pow(size,2) + 1) / 2;
+
+        for (int i = 0; i < size; i++) {
+            sum_d1 += s.board[i][i];
+            sum_d2 += s.board[i][size-i-1];
+            for (int j = 0; j < size; j++) {
+               sum_col += s.board[i][j];
+               sum_row += s.board[j][i];
+            }
+            if (sum_col != sumaMagica || sum_row != sumaMagica
+                || sum_d1 != sumaMagica || sum_d2 != sumaMagica)
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Accion[] acciones(Estado es) {
+        ArrayList<Accion> accAL = new ArrayList<>();
+        EstadoCuadradoMagico s = (EstadoCuadradoMagico)es;
+        int size = s.size;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(s.board[i][j] == 0){
+                    for (int n = 1; n < size; n++){
+                        Accion accion = new AccionCuadradoMagico(n, i, j);
+                        if (accion.esAplicable(es))
+                            accAL.add(accion);
+
+                    }
+                }
+            }
+        }
+        //Convertimos el ArrayList a Array
+        Accion[] accA = new Accion[accAL.size()];
+        accA = accAL.toArray(accA);
+        return accA;
+    }
 }
